4c4692b7be939cf32901343d5c8d70de7be0cf3e
Hack Reactor Student <student@hackreactor.com>
2017-10-25 15:31:34 -0700

    Added mouse hover functionality, still need to bold

--- app.js
+++ app.js
@@ -4,20 +4,44 @@ class GroceryListItem extends React.Component {
 
   constructor(props) {
     super(props);
+    
+    this.state = {
+      done: false
+    }
+    
+  }
+  
+  onMouseEnter() {
+    this.setState({
+      done: true
+    });
+  }
+  
+  onMouseLeave() {
+    this.setState({
+      done: false
+    });
   }
 
   render() {
+    
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+    //Change font weight for bolding effect?
 
     return (
-      <li>{this.props.item}</li>
+      <li style={style} onMouseEnter={this.onMouseEnter.bind(this)} onMouseLeave={this.onMouseLeave.bind(this)}>{this.props.item}</li>
     );
   }
 }
 
+
+
 var GroceryList = (props) => (
   <div>
   	<h2>
-  	  My Grocery List
+  	  Our Grocery List
   	</h2>
   	<ul>
   	  {props.groceryItems.map(item => 
@@ -28,6 +52,39 @@ var GroceryList = (props) => (
 );
 
 ReactDOM.render(<GroceryList groceryItems = {['water', 'juice', 'soda', 'drink']} />, document.getElementById("app"));
+
+
+
+// class GroceryListItem extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+
+//     return (
+//       <li>{this.props.item}</li>
+//     );
+//   }
+// }
+
+// var GroceryList = (props) => (
+//   <div>
+//     <h2>
+//       Our Grocery List
+//     </h2>
+//     <ul>
+//       {props.groceryItems.map(item => 
+//         <GroceryListItem item={item} /> 
+//       )}
+//     </ul>
+//   </div>
+// );
+
+// ReactDOM.render(<GroceryList groceryItems = {['water', 'juice', 'soda', 'drink']} />, document.getElementById("app"));
+
+
 
 
 /*
